3e8953cc95111bf7ff93943a5db76eeff450f066
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-10 01:18:17 +0530

    Update Configuration.js

--- Configuration.js
+++ Configuration.js
@@ -1,2 +1,2 @@
 const mongoose =require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/MyProject');
+mongoose.connect('mongodb+srv://yatharthpatel014:Yatharth@timecluster.g73ta.mongodb.net/?retryWrites=true&w=majority&appName=timecluster');
